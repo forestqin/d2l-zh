bbceb5cf5296fca12adbbe7029620846b21cab57
forestqin <qintao361@126.com>
2023-07-04 07:33:28 +0800

    kaggle house price v3

---
+++ 01_tao_learning/kaggle_house_price.py
@@ -0,0 +1,19 @@
+import os
+import sys
+import pandas as pd
+import numpy as np
+
+os.chdir(sys.path[0])
+
+def preprocess(df):
+    df1 = df.copy()
+    df1
+
+
+def main():
+    df_train = pd.read_csv("./data/kaggle_house_price/kaggle_house_pred_train.csv")
+    print(df_train.head())
+    df2 = 
+
+if __name__ == "__main__":
+    main()
